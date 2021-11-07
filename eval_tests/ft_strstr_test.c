c4895cbf8642993abf01366e09c191af8e7c079f
Jaakko Saarinen <jsaarine@student.hive.fi>
2021-11-07 18:27:29 +0200

    strstr testing

--- eval_tests/ft_strstr_test.c
+++ eval_tests/ft_strstr_test.c
@@ -6,11 +6,15 @@
 /*   By: jsaarine <jsaarine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 17:35:39 by jsaarine          #+#    #+#             */
-/*   Updated: 2021/11/07 17:37:23 by jsaarine         ###   ########.fr       */
+/*   Updated: 2021/11/07 17:58:49 by jsaarine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../libft.h"
+#include <stdio.h>
+
 int	main(void)
 {
-	
+	printf("REF:  ", strstr("Hello","el"));
+	printf("mine: ", ft_strstr("Hello","el"));
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jsaarine <jsaarine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 17:17:40 by jsaarine          #+#    #+#             */
-/*   Updated: 2021/11/07 15:44:34 by jsaarine         ###   ########.fr       */
+/*   Updated: 2021/11/07 18:02:34 by jsaarine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <string.h>
 
+char	*ft_strstr(const char *a, const char *b);
 int	ft_strncmp(const char *a, const char *b, size_t n);
 int ft_strcmp(const char *a, const char *b);
 int ft_atoi(const char *str);
