242ef377390211e11f78f2b37d0af2550fe4a0c1
Jaakko Saarinen <jsaarine@student.hive.fi>
2021-11-13 18:01:32 +0200

    fix norm on strcat & strncat

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,7 +6,7 @@
 /*   By: jsaarine <jsaarine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 15:49:39 by jsaarine          #+#    #+#             */
-/*   Updated: 2021/11/13 17:36:09 by jsaarine         ###   ########.fr       */
+/*   Updated: 2021/11/13 17:57:46 by jsaarine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 char	*ft_strcat(char *dest, const char *src)
 {
-	size_t destlen;
-	size_t srclen;
+	size_t	destlen;
+	size_t	srclen;
 
 	destlen = ft_strlen(dest);
 	srclen = ft_strlen(src);
