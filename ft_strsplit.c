7ece50fc83c8ab9cc92b4afc55426a4425d05c84
Jaakko Saarinen <jsaarine@student.hive.fi>
2021-11-18 15:46:04 +0200

    work on strplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: jsaarine <jsaarine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 12:52:27 by jsaarine          #+#    #+#             */
-/*   Updated: 2021/11/18 15:18:03 by jsaarine         ###   ########.fr       */
+/*   Updated: 2021/11/18 15:45:33 by jsaarine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,27 @@ int ft_wordcount(char const *s, char c, int wordcount)
 char	**ft_wordmalloc(char const *s, char c, int wordcount, char **words)
 {
 	int	i;
+	int	wordlen;
+	char *wordstart;
 
 	i = 0;
 	while (i < wordcount)
 	{
-		if (c)
-			c++;
-		s++;
+		while (*s == c)
+			s++;
+		if (*s != c)
+		{
+			i++;
+			wordlen = 0;
+			wordstart = s;
+			while (*s != c)
+			{	
+				s++;
+				wordlen++;
+			}
+		}
+
+		mallocword!!!
 	}
 	return (words);
 }
