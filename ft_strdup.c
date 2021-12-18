d7598e91fd8a25c1349debad61a48939bf886a96
Jaakko Saarinen <pod@fea.st>
2021-12-19 00:39:10 +0200

    shorten ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -15,20 +15,13 @@
 
 char	*ft_strdup(const char *src)
 {
-	int		i;
-	int		len;
 	char	*dest;
 
-	len = ft_strlen(src);
-	dest = (char *) malloc(sizeof(char) * (len + 1));
+	dest = (char *) malloc(sizeof(char) * (ft_strlen(src) + 1));
 	if (!dest)
 		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
 	return (dest);
 }
