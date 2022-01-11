742b0f5a2d72890856af1bb2dc950b6ca68e22ce
Jaakko Saarinen <pod@fea.st>
2022-01-11 13:25:41 +0200

    fix vec_resize len

--- vec_resize.c
+++ vec_resize.c
@@ -9,9 +9,15 @@ int	vec_resize(t_vec *src, size_t target_size)
 	if (vec_new(&resized, target_size, src->elem_size) == -1)
 		return (-1);
 	if (src->len > target_size)
+	{
 		ft_memcpy(resized.memory, src->memory, target_size * src->elem_size);
+		resized.len = target_size;
+	}
 	else
+	{
 		ft_memcpy(resized.memory, src->memory, src->alloc_size);
+		resized.len = src->len;
+	}
 	vec_free(src);
 	*src = resized;
 	return (1);
