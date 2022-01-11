ce6e5a2217964bbe5df36f2c0a0b8a3b52e2cd66
Jaakko Saarinen <pod@fea.st>
2022-01-11 13:12:51 +0200

    vec_resize passes tests

--- vec_resize.c
+++ vec_resize.c
@@ -8,8 +8,10 @@ int	vec_resize(t_vec *src, size_t target_size)
 		return (-1);
 	if (vec_new(&resized, target_size, src->elem_size) == -1)
 		return (-1);
-	if (vec_from(&resized, src, target_size, src->elem_size) == -1)
-		return (-1);
+	if (src->len > target_size)
+		ft_memcpy(resized.memory, src->memory, target_size * src->elem_size);
+	else
+		ft_memcpy(resized.memory, src->memory, src->alloc_size);
 	vec_free(src);
 	*src = resized;
 	return (1);
