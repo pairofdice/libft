746064fba2f07571f924aa5be05516c6e37600dc
Jaakko Saarinen <pod@fea.st>
2022-01-20 16:28:26 +0200

    Add vec_append

--- vec_append.c
+++ vec_append.c
@@ -6,7 +6,7 @@
 /*   By: jsaarine <jsaarine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 20:03:52 by jsaarine          #+#    #+#             */
-/*   Updated: 2022/01/12 20:10:24 by jsaarine         ###   ########.fr       */
+/*   Updated: 2022/01/20 16:27:55 by jsaarine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,22 @@
 */
 int	vec_append(t_vec *dst, t_vec *src)
 {
+	size_t	space_needed;
+
+	if (!dst || !src || !src->memory)
+		return (-1);
+	if (!dst->memory)
+		vec_new(dst, 1, dst->elem_size);
+	space_needed = dst->len * dst->elem_size + src->len * src->elem_size;
+	if (dst->alloc_size < space_needed)
+	{
+		if (vec_resize(dst, space_needed * 2) == -1)
+			return (-1);
+	}
+	memcpy(
+		&dst->memory[dst->len * dst->elem_size],
+		src->memory,
+		src->len * src->elem_size);
+	dst->len += src->len;
 	return (1);
 }
